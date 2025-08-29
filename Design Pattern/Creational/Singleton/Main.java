fc53492982b6582c09ba16091f7858c1c0081fd3
Jayant Gupta <jayant.gupta.dln@gmail.com>
2025-08-29 18:30:01 +0530

    Single threaded Singleton pattern impementation

--- Design Pattern/Creational/Singleton/Main.java
+++ Design Pattern/Creational/Singleton/Main.java
@@ -2,8 +2,23 @@
 public class Main {
     public static void main(String[] args) {
 
-        Singleton s1 = Singleton.getInstance();
-        Singleton s2 = Singleton.getInstance();
+        // This is where it fails; if we run it in multi-threaded environment
+
+        Runnable task1 = () -> {
+            SingleThreadSingleton s1 = SingleThreadSingleton.getInstance("Foo");
+            System.out.println(s1.value);
+        };
+        
+        Runnable task2 = () -> {
+            SingleThreadSingleton s2 = SingleThreadSingleton.getInstance("Bar");
+            System.out.println(s2.value);
+        };
+
+        Thread t1 = new Thread(task1);
+        Thread t2 = new Thread(task2);
+
+        t1.start();
+        t2.start();
 
         System.out.println("Finish");
     }
---
+++ Design Pattern/Creational/Singleton/SingleThreadSingleton.java
@@ -0,0 +1,18 @@
+
+// This only works in Single Threaded environment
+public class SingleThreadSingleton {
+    
+    private static SingleThreadSingleton instance;
+    public String value;
+    private SingleThreadSingleton(String value) {
+        this.value = value;
+    }
+
+    public static SingleThreadSingleton getInstance(String value) {
+        if(instance == null) {
+            System.out.println("Creating an instance of Singleton class");
+            instance = new SingleThreadSingleton(value);
+        }
+        return instance;
+    }
+}
